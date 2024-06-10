edff72b64c5a2c077cadfef72122324c13e66bfd
valentinefleith <valentine.fleith@gmail.com>
2024-06-10 16:27:54 +0200

    adapt script to the whole corpus

--- feature_extraction/wav_conversion/mp4_to_wav.py
+++ feature_extraction/wav_conversion/mp4_to_wav.py
@@ -12,22 +12,70 @@ pydub library is required.
 import sys
 import os
 import glob
+import pandas as pd
 from pydub import AudioSegment
 
+PARTS = ["beg", "end", "mid", "full"]
+ACCENTS = {
+    "è": "e",
+    "é": "e",
+    "ë": "e",
+    "é": "e",
+    "ç": "c",
+    "É": "E",
+    "Á": "A",
+    "ï": "i",
+}
 
-def save_as_wav_files(video_paths, dest_path):
+
+def find_right_name(filepath, dest_path, df_ids):
+    filename = filepath.split("/")[-1]
+    filepart = filepath.split("/")[-2]
+    for accent, no_accent in ACCENTS.items():
+        if accent in filename:
+            filename = filename.replace(accent, no_accent)
+    if filepart == "original":
+        return None
+    for part in PARTS:
+        if filepath.split("/")[-2].startswith(part):
+            dest_path += f"{part}/"
+    try:
+        name_path = df_ids.query("`Name` == @filename")["subname"].values[0]
+    except IndexError:
+        print(f"\nERROR: {filename} not found.\n")
+        return None
+    dest_path += name_path
+    return dest_path + ".wav"
+
+
+def save_as_wav_files(video_paths, dest_path, df_ids):
     os.makedirs(dest_path, exist_ok=True)
+    for part in PARTS:
+        os.makedirs(dest_path + part, exist_ok=True)
+    df_ids = pd.concat(
+        [
+            df_ids.rename(str.lower, axis="columns"),
+            df_ids["Name"].str.replace("_transcript.txt", ".mp4"),
+        ],
+        axis=1,
+    )
     for filepath in video_paths:
-        name = filepath.split("/")[-1].split(".")[0]
+        name = find_right_name(filepath, dest_path, df_ids)
+        if name is None:
+            continue
+        print(name, end="\n")
+        # name = filepath.split("/")[-1].split(".")[0]
         sound = AudioSegment.from_file(filepath, format="mp4")
-        sound.export(f"{dest_path}/{name}.wav", format="wav")
+        sound.export(name, format="wav")
 
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 mp4_to_wav.py /path/do/video_dir")
-    video_paths = glob.glob(f"{sys.argv[1]}/mp4/*.mp4")
-    save_as_wav_files(video_paths, f"{sys.argv[1]}/wav")
+    id_path = f"{sys.argv[1]}/transcripts_ID_list_modif.csv"
+    video_paths = glob.glob(f"{sys.argv[1]}/mp4/*/*/*.mp4")
+    df_ids = pd.read_csv(id_path, sep=";")
+    save_as_wav_files(video_paths, f"{sys.argv[1]}/wav/", df_ids)
 
 
 if __name__ == "__main__":
